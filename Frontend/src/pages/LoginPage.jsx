33dfe1e8c375f8e3d420ece2f7daea7e7f74b884
Asus <agavindu123@gmail.com>
2025-07-03 15:01:30 +0530

    update LoginPage component: enhance layout and add user authentication form

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -5,22 +5,87 @@ const LoginPage = () => {
     <>
       <div className="flex h-screen">
         <div className="w-1/2 bg-black p-4 flex items-center justify-center">
-            <h1 className="text-4xl font-bold text-center text-blue-50 underline tracking-wide">Welcome Back</h1>
+          <h1 className="text-4xl font-bold text-center text-blue-50 underline tracking-wide">
+            Welcome Back
+          </h1>
         </div>
         <div className="w-1/2 bg-white-50 p-4 flex items-center justify-center">
-            <div className="max-w-md w-full space-y-8">
-                <div className="text-center">
-                    <h2 className="mt-6 text-4xl font-extrabold text-gray-900">Welcome Back</h2>
-                    <p className="mt-2 text-sm text-gray-600">Sign in to your account</p>
-                </div>
-                <form className="mt-8 space-y-6" action="#" method="post">
-                  <div><label htmlFor="email" className="block text-sm font-medium text-gray-700">Email address</label>
-                  <input id="email" name="email" type="email" autoComplete="email" required className="mt-1 block w-full px-4 py-2 border border-gray-700 rounded-md shadow-sm foucs:ring-indigo-500 focus:border-ingigo-500 sm:text-sm" /></div>
-
-                  <div><label htmlFor="Password" className="block text-sm font-medium text-gray-700">Password</label>
-                  <input id="password " name="password" type="password" required className="mt-1 block w-full px-4 py-2 border border-gray-700 rounded-md shadow-sm foucs:ring-ingigo-500 foucus:border-ingigo-500 sm:text-sm"/></div>
-                </form> 
+          <div className="max-w-md w-full space-y-8">
+            <div className="text-center">
+              <h2 className="mt-6 text-4xl font-extrabold text-gray-900">
+                Welcome Back
+              </h2>
+              <p className="mt-2 text-sm text-gray-600">
+                Sign in to your account
+              </p>
             </div>
+            <form className="mt-8 space-y-6" action="#" method="post">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Email address
+                </label>
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="mt-1 block w-full px-4 py-2 border border-gray-700 rounded-md shadow-sm foucs:ring-indigo-500 focus:border-ingigo-500 sm:text-sm"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="Password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
+                <input
+                  id="password "
+                  name="password"
+                  type="password"
+                  required
+                  className="mt-1 block w-full px-4 py-2 border border-gray-700 rounded-md shadow-sm foucs:ring-ingigo-500 foucus:border-ingigo-500 sm:text-sm"
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                  <input
+                    id="remember-me"
+                    name="remember-me"
+                    type="checkbox"
+                    className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                  />
+                  <label
+                    htmlFor="remember-me"
+                    className="ml-2 block text-sm text-gray-900"
+                  >
+                    Remember me
+                  </label>
+                </div>
+
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-medium text-indigo-600 hover:text-indigo-500"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+              </div>{" "}
+              <div>
+                <button
+                  type="submit"
+                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-500 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Sign In
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </>
