719663a1c468a04fd7f0e8b5a45e1d1cadd23a9d
Gavinduachintha <agavindu123@gmail.com>
2026-07-07 17:07:35 +0530

    feat(ui): create LoginBox component for user authentication with form fields and social login options

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import RegisterBox from './registerBox';
+import LoginBox from './registerBox';
 const RegisterPage = () => {
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="h-full min-h-screen grid grid-cols-1 lg:grid-cols-2 bg-[#0D0D0D] overflow-hidden">
+      {" "}
+      {/* Left Section */}
+      <div className="flex items-center justify-center">
+        <RegisterBox />
+      </div>
+      {/* Right Section */}
+      <div className="hidden lg:flex items-center justify-center bg-[#6C47FF]">
+        {/* Hero Image / Illustration / Branding */}
+        <h1 className="text-5xl font-bold text-white">Welcome Back</h1>
+      </div>
     </div>
   );
 };
